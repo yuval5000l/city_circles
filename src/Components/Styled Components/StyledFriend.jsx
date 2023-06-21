b4d4740906bf8de978d2e0d68edcd26625880262
talmich <talmichaeli77@gmail.com>
2023-06-21 15:24:33 +0300

    created the friends page

---
+++ src/Components/Styled Components/StyledFriend.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Stack, Typography, Box } from '@mui/material';
+import SupervisedUserCircleIcon from '@mui/icons-material/SupervisedUserCircle';
+import {StyledCirclesIcon} from "./styledComponents";
+import {StyledBusinessFeed} from "./styledComponents";
+import theme from "../../Theme/Theme";
+export default function StyledFriend({ user_id, user_name = 'name', profile_photo_url = '', circles = [] }) {
+    const circlesArray = Array.isArray(circles) ? circles : [circles];
+
+    return (
+        <Box sx={{ width: '100%', padding: '0.4rem', boxShadow: '0 2px 5px rgba(0, 0, 0, 0.3)', borderBottom: '0.1rem solid #C3ED5B' }}>
+            <Stack direction="row" spacing={1}>
+                <Box>
+                    <Stack direction="column" spacing={2}>
+                        <Stack direction="row" spacing={1} justifyContent="flex-start" alignItems="flex-start">
+                            <Typography variant="h3" textAlign="left">{user_name}</Typography>
+                        </Stack>
+                        <Stack direction="row" spacing={1} justifyContent="flex-start" alignItems="flex-start">
+                            <StyledBusinessFeed /> {/* Add the StyledBusinessFeed component here */}
+                            <Stack direction="column" spacing={1}>
+                                {circlesArray.map((circle, index) => (
+                                    <Typography variant="body1" key={index}>
+                                        <Box component="span" sx={{ display: 'flex', alignItems: 'center' }}>
+                                            <SupervisedUserCircleIcon sx={{ marginRight: '0.5rem',color: theme.palette.primary.main }} />
+                                            {circle}
+                                        </Box>
+                                    </Typography>
+                                ))}
+                            </Stack>
+                        </Stack>
+                    </Stack>
+                </Box>
+            </Stack>
+        </Box>
+    );
+}
--- src/routes/friends/FriendsPage.component.jsx
+++ src/routes/friends/FriendsPage.component.jsx
@@ -1,13 +1,43 @@
-import {Component} from "react";
+import React, { useState } from 'react';
+import { List, ListItem, Box, TextField, Typography } from '@mui/material';
+import StyledFriend from "../../Components/Styled Components/StyledFriend";
+import theme from "../../Theme/Theme";
+const FriendsPageComponent = () => {
+    const [searchText, setSearchText] = useState('');
+    const friends = [
+        { id: 1, name: 'Yuvie Laval', circles: ['Huji', 'Gavram','Matar'] },
+        { id: 2, name: 'Gold Guystien', circles: ['Huji','Gavram','Matar'] },
+        { id: 3, name: 'Pele Dila', circles: ['Huji','Harhats','Matar'] },
+    ];
 
+    const handleSearchChange = (event) => {
+        setSearchText(event.target.value);
+    };
 
-class FriendsPageComponent extends Component{
+    const filteredFriends = friends.filter((friend) =>
+        friend.name.toLowerCase().includes(searchText.toLowerCase())
+    );
 
-    render(){
-        // const position = [31.777587, 35.215094]; //[this.state.location.lat, this.state.location.lng];
-        return(
-            <div> Friends </div>
-        );
-    }
-}
+    return (
+        <Box sx={{ width: '100%', bgcolor: 'background.paper', paddingTop: '0.37rem' }}>
+            <TextField
+                label="Search Friends"
+                value={searchText}
+                onChange={handleSearchChange}
+                fullWidth
+            />
+            <Typography variant="h6" sx={{color: theme.palette.primary.main}} >
+                {filteredFriends.length} Friends
+            </Typography>
+            <List>
+                {filteredFriends.map((friend) => (
+                    <ListItem key={friend.id}>
+                        <StyledFriend user_id={friend.id} user_name={friend.name} circles={friend.circles} />
+                    </ListItem>
+                ))}
+            </List>
+        </Box>
+    );
+};
+
 export default FriendsPageComponent;
